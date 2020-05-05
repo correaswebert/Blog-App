a8c049d6e1c659c2939ef83d6e9e3c46735ad828
Swebert Correa <correaswebert@gmail.com>
2020-05-05 19:56:06 +0530

    updating profiles now possible, also images are scaled down to 300x300

--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 
 def register(request):
@@ -35,4 +35,33 @@ def register(request):
 @login_required
 def profile(request):
     """allow users to update their profile"""
-    return render(request, 'users/profile.html')
+
+    if request.method == 'POST':
+        # pre-populate the fields to current values
+        # by passing `instance` to the form, and also pass the newly
+        # updated values
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        # `FILES` is needed for media files (image in our case)
+        p_form = ProfileUpdateForm(request.POST,
+                                   request.FILES,
+                                   instance=request.user.profile)
+
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+
+            messages.success(request, f'Account has been updated!')
+
+            # we won't render the profile page as another POST request will
+            # be sent, prompting user to confirm reload of the page
+            # redirect send a GET request
+            return redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form,
+    }
+    return render(request, 'users/profile.html', context)
